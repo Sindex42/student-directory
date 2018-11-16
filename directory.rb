6cf8a4a79f163313224046f33df763412a73fca3
Connor Kam-Cheong <connorkc42@gmail.com>
2018-11-16 18:52:19 +0000

    Added print_by_cohort

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 @students = []
+@cohort_list = []
 @longest_name = 0
 @line_width = 40
 
@@ -63,20 +64,22 @@ def input_students
 end
 
 
+  def add_name_spacing(shift = 0, category = 0)
+    (@longest_name - category + shift).times { print " " }
+  end
+
 def print_header
   puts "The students of Villains Academy".center(@line_width)
   puts "-------------".center(@line_width)
   print "Nr Name"
-  # whitespace pading for "Cohort"
-  (@longest_name - 2).times { print " " }
+  add_name_spacing(-2)
   puts "Cohort           Country Height"
 end
 
 def print_students_list(students)
   students.each_with_index do |student, student_number|
     print "#{student_number + 1}. #{student[:name]}"
-    # whitespace padding
-    (@longest_name - student[:name].length + 1).times { print " " }
+    add_name_spacing(1, student[:name].length)
     puts(
       "(#{student[:cohort]} cohort) "\
       "#{student[:birth_country]} "\
@@ -93,25 +96,42 @@ end
     end
   end
 
-  def print_students_by_letter(students)
+  def print_students_by_letter(students, letter)
     students.each_with_index do |student, index|
-      if student[:name].start_with?("D")
+      if student[:name].start_with?(letter)
         puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
       end
     end
   end
 
-  def print_students_by_length(students)
+  def print_students_by_length(students, length)
     list.each_with_index do |student, index|
-      if student[:name].length <= 12
+      if student[:name].length <= length
         puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
       end
     end
   end
 
   def print_students_by_cohort(students)
-
+    list_all_cohorts()
+    @cohort_list.each do |cohort|
+      students.each_with_index do |student, student_number|
+        if student[:cohort] == cohort
+          print "#{student_number + 1}. #{student[:name]}"
+          add_name_spacing(1, student[:name].length)
+          puts(
+            "(#{student[:cohort]} cohort) "\
+            "#{student[:birth_country]} "\
+            "#{student[:height].to_i}cms"
+            )
+        end
+      end
+    end
   end
+
+    def list_all_cohorts
+      @cohort_list = (@students.map { |student| student[:cohort] }).uniq
+    end
 
 def print_footer(students)
   case @students.length
@@ -129,7 +149,8 @@ input_students()
 
 unless @students.empty?
   print_header()
-  print_students_list(@students)
+  #print_students_list(@students)
+  print_students_by_cohort(@students)
 end
 
 print_footer(@students)
