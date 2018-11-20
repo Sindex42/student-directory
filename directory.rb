c3781c3443a1ad270b900ec14839c7477ffd7f88
Connor Kam-Cheong <connorkc42@gmail.com>
2018-11-20 15:16:36 +0000

    'directory.rb' now loads 'students.csv' by default or another file if specified

--- directory.rb
+++ directory.rb
@@ -225,7 +225,7 @@ end
 
   def try_load_students
     filename = ARGV.first # first argument from the command line
-    return if filename.nil? # get out of the methood is it isn't given
+    filename = "students.csv" if filename.nil? 
     if File.exists?(filename) # if it exists
       load_students(filename)
       puts "Loaded #{@students.count} from #{filename}"
